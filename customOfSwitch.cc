27c3e921063c9c6eb73b771753593954a521d35a
Jesse <jesseguerrero1991@gmail.com>
2022-10-01 15:48:13 -0500

    Testing something

--- customOfSwitch.cc
+++ customOfSwitch.cc
@@ -39,118 +39,92 @@
 
 using namespace ns3;
 
+Ptr<OFSwitch13InternalHelper> of13Helper = CreateObject<OFSwitch13InternalHelper> ();
+NodeContainer switches;
+InternetStackHelper internet; // Install the TCP/IP stack into hosts nodes
+CsmaHelper csmaHelper;
+NetDeviceContainer hostDevices;
+NetDeviceContainer switchPorts;
 void SetAllNodesXY(NodeContainer nodes, double x, double y, double deltaX);
 void SetNodeXY(Ptr<Node> node, double x, double y);
+void SetupSwitch(NodeContainer hosts);
+void SetupIpv4Addresses();
+void InstallPing(Ptr<Node> src, Ptr<Node> dest);
 
 int
 main (int argc, char *argv[])
 {
-  uint16_t simTime = 38;
-  bool verbose = false;
-  bool trace = false;
+	uint16_t simTime = 38;
+	bool verbose = false;
+	bool trace = false;
 
-  // Configure command line parameters
-  CommandLine cmd;
-  cmd.AddValue ("simTime", "Simulation time (seconds)", simTime);
-  cmd.AddValue ("verbose", "Enable verbose output", verbose);
-  cmd.AddValue ("trace", "Enable datapath stats and pcap traces", trace);
-  cmd.Parse (argc, argv);
+	// Configure command line parameters
+	CommandLine cmd;
+	cmd.AddValue ("simTime", "Simulation time (seconds)", simTime);
+	cmd.AddValue ("verbose", "Enable verbose output", verbose);
+	cmd.AddValue ("trace", "Enable datapath stats and pcap traces", trace);
+	cmd.Parse (argc, argv);
 
-  if (verbose)
-    {
-      OFSwitch13Helper::EnableDatapathLogs ();
-      LogComponentEnable ("OFSwitch13Interface", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13Device", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13Port", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13Queue", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13SocketHandler", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13Controller", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13LearningController", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13Helper", LOG_LEVEL_ALL);
-      LogComponentEnable ("OFSwitch13InternalHelper", LOG_LEVEL_ALL);
-    }
+	if (verbose)
+	{
+	  OFSwitch13Helper::EnableDatapathLogs ();
+	  LogComponentEnable ("OFSwitch13Interface", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13Device", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13Port", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13Queue", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13SocketHandler", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13Controller", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13LearningController", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13Helper", LOG_LEVEL_ALL);
+	  LogComponentEnable ("OFSwitch13InternalHelper", LOG_LEVEL_ALL);
+	}
 
-  // Enable checksum computations (required by OFSwitch13 module)
-  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
+	// Enable checksum computations (required by OFSwitch13 module)
+	GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
 
-  // Create two host nodes
-  NodeContainer hosts;
-  hosts.Create (2);
+	switches.Create(3);
+	csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
+	csmaHelper.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (500)));
 
-  // Create the switch node
-  Ptr<Node> switchNode = CreateObject<Node> ();
+	// Create the controller node
+	Ptr<Node> controllerNode = CreateObject<Node> ();
+	Names::Add("Controller", controllerNode);
 
-  // Create switch 2
-  //Ptr<Node> switchNode2 = CreateObject<Node> ();
+	// Configure the OpenFlow network domain
+	of13Helper->InstallController (controllerNode);
 
-  // Use the CsmaHelper to connect host nodes to the switch node
-  CsmaHelper csmaHelper;
-  csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
-  csmaHelper.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (500)));
+	//Southbound Network
+	NodeContainer hosts1, hosts2, hosts3;
+	hosts1.Create (3); hosts2.Create(3); hosts3.Create(3);
+	SetupSwitch(hosts1);
+	SetupSwitch(hosts2);
+	SetupSwitch(hosts3);
 
-  NetDeviceContainer hostDevices;
-  NetDeviceContainer switchPorts;
-  for (size_t i = 0; i < hosts.GetN (); i++)
-    {
-      NodeContainer pair (hosts.Get (i), switchNode);
-      NetDeviceContainer link = csmaHelper.Install (pair);
-      hostDevices.Add (link.Get (0));
-      switchPorts.Add (link.Get (1));
-    }
+	//OfSwitch Config
+	of13Helper->SetChannelType(OFSwitch13Helper::ChannelType::DEDICATEDP2P);
+	of13Helper->CreateOpenFlowChannels ();
 
-  // Create the controller node
-  Ptr<Node> controllerNode = CreateObject<Node> ();
-  Names::Add("Controller", controllerNode);
+	SetupIpv4Addresses();
+	//InstallPing(hosts1.Get(0), hosts2.Get(0));
 
-  // Configure the OpenFlow network domain
-  Ptr<OFSwitch13InternalHelper> of13Helper = CreateObject<OFSwitch13InternalHelper> ();
-  of13Helper->InstallController (controllerNode);
-  of13Helper->InstallSwitch (switchNode, switchPorts);
-  //of13Helper->InstallSwitch(switchNode2);
-  of13Helper->SetChannelType(OFSwitch13Helper::ChannelType::DEDICATEDCSMA);
-  of13Helper->CreateOpenFlowChannels ();
+	//Visuals
+	SetNodeXY(controllerNode, 15, 5);
+	SetAllNodesXY(switches, 5, 12.5, 7.5);
+	AnimationInterface anim("OfExampleAnim.xml");
 
-  // Install the TCP/IP stack into hosts nodes
-  InternetStackHelper internet;
-  internet.Install (hosts);
+	// Enable datapath stats and pcap traces at hosts, switch(es), and controller(s)
+	if (trace)
+	{
+	  of13Helper->EnableOpenFlowPcap ("openflow");
+	  of13Helper->EnableDatapathStats ("switch-stats");
+	  csmaHelper.EnablePcap ("switch", switchPorts, true);
+	  csmaHelper.EnablePcap ("host", hostDevices);
+	}
 
-  // Set IPv4 host addresses
-  Ipv4AddressHelper ipv4helpr;
-  Ipv4InterfaceContainer hostIpIfaces;
-  ipv4helpr.SetBase ("10.1.1.0", "255.255.255.0");
-  hostIpIfaces = ipv4helpr.Assign (hostDevices);
-
-  // Configure ping application between hosts
-
-  std::ostringstream out;
-  hostIpIfaces.GetAddress(1).Print(out);
-  NS_LOG_UNCOND (out.str().c_str());
-  V4PingHelper pingHelper = V4PingHelper ("10.1.1.2");
-  pingHelper.SetAttribute ("Interval", TimeValue (Seconds (8.0)));
-  pingHelper.SetAttribute ("Verbose", BooleanValue (true));
-
-  ApplicationContainer pingApps = pingHelper.Install (hosts.Get (0));
-  pingApps.Start (Seconds (0));
-
-  SetAllNodesXY(hosts, 3, 20, 1);
-  SetNodeXY(controllerNode, 15, 5);
-  SetAllNodesXY(switches, 5, 12.5, 7.5);
-
-  AnimationInterface anim("OfExampleAnim.xml");
-
-  // Enable datapath stats and pcap traces at hosts, switch(es), and controller(s)
-  if (trace)
-    {
-      of13Helper->EnableOpenFlowPcap ("openflow");
-      of13Helper->EnableDatapathStats ("switch-stats");
-      csmaHelper.EnablePcap ("switch", switchPorts, true);
-      csmaHelper.EnablePcap ("host", hostDevices);
-    }
-
-  // Run the simulation
-  Simulator::Stop (Seconds (simTime));
-  Simulator::Run ();
-  Simulator::Destroy ();
+	// Run the simulation
+	Simulator::Stop (Seconds (simTime));
+	Simulator::Run ();
+	Simulator::Destroy ();
 }
 
 void SetAllNodesXY(NodeContainer nodes, double x, double y, double deltaX) {
@@ -178,4 +152,34 @@ void SetNodeXY(Ptr<Node> node, double x, double y) {
 	mobileHosts.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
 	mobileHosts.Install (node);
 	//anim.SetConstantPosition(node, x, y, 0);
+}
+
+void SetupSwitch(NodeContainer hosts) {
+	for (size_t i = 0; i < hosts.GetN (); i++)
+	{
+	  NodeContainer pair (hosts.Get (i), switches.Get(2));
+	  NetDeviceContainer link = csmaHelper.Install (pair);
+	  hostDevices.Add (link.Get (0));
+	  switchPorts.Add (link.Get (1));
+	}
+	of13Helper->InstallSwitch (switches.Get(2), switchPorts);
+	internet.Install (hosts);
+	SetAllNodesXY(hosts, 17, 20, 1);
+}
+
+void SetupIpv4Addresses() {
+	Ipv4AddressHelper ipv4helpr;
+	Ipv4InterfaceContainer hostIpIfaces;
+	ipv4helpr.SetBase ("10.1.1.0", "255.255.255.0");
+	hostIpIfaces = ipv4helpr.Assign (hostDevices);
+}
+
+void InstallPing(Ptr<Node> src, Ptr<Node> dest) {
+	// Configure ping application between hosts
+	V4PingHelper pingHelper = V4PingHelper (dest->GetObject<Ipv4>()->GetAddress(1,0).GetLocal());
+	pingHelper.SetAttribute ("Interval", TimeValue (Seconds (100)));
+	pingHelper.SetAttribute ("Verbose", BooleanValue (true));
+
+	ApplicationContainer pingApps = pingHelper.Install(src);
+	pingApps.Start (Seconds (3));
 }
